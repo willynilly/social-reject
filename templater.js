c5ed6c6e8d7850b63e58d3cb00262da44c0e86ff
Will Riley <wanderingwill@gmail.com>
2017-03-12 23:44:34 -0400

    Fixed template folder location for global command.

--- templater.js
+++ templater.js
@@ -1,4 +1,6 @@
 const jsrender = require('jsrender');
+const path = require('path');
+const relPath = './' + path.relative(process.cwd(), path.dirname(require.main.filename));
 
 function print(templateNameOrString, templateData, isString) {
     if (Array.isArray(templateNameOrString)) {
@@ -6,11 +8,14 @@ function print(templateNameOrString, templateData, isString) {
             print(e, templateData, isString);
         })
     } else if (templateNameOrString != '' && templateNameOrString !== null && templateNameOrString !== undefined) {
-        let templateFilePathOrString = !isString ? './templates/' + templateNameOrString + '.txt' : templateNameOrString
+        let templateFilePathOrString = !isString ? relPath + '/templates/' + templateNameOrString + '.txt' : templateNameOrString
         let template = jsrender.templates(templateFilePathOrString);
+
         let rendered = template.render(templateData);
+
         console.log(rendered);
         console.log('');
+
     } else {
         console.log('');
     }
